a12a6dfb1d6728605336cb75b061003c2c808b08
Ryan Dawkes <ryandawkes7@gmail.com>
2020-11-16 22:43:15 +0000

    Architecture Reconstruction with Secure Linkage

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://aerospace-admin:BristolAerospace1234@aerospacedb.al5th.mongodb.net/AerospaceDB?retryWrites=true&w=majority',
-    secretOrKey: "secret"
-}
